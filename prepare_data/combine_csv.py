6481ee19ed950718cb9d43473d20d102a4b08031
korneltlaczala <kornel.tlaczala@gmail.com>
2024-01-05 00:08:08 +0100

    repair combine_csv.py

--- prepare_data/combine_csv.py
+++ prepare_data/combine_csv.py
@@ -1,25 +1,31 @@
 import os
 import pandas as pd
 
-def combine_csv():
-    for csv_file in os.listdir(csv_dir_path):
-        print("working on file " + csv_file + "....")
+def combine_csv(csv_dir_path):
+
+    dataframes_list = []
+    csv_data_dir_path = os.path.join(csv_dir_path, "people")
+
+    for csv_file in os.listdir(csv_data_dir_path):
+        # print("working on file " + csv_file + "....")
         if csv_file.endswith('.csv'):
-            csv_file_path = os.path.join(csv_dir_path, csv_file)
+            csv_file_path = os.path.join(csv_data_dir_path, csv_file)
             current_data = pd.read_csv(csv_file_path)
             dataframes_list.append(current_data)
-        print(current_data)
 
     combined_data = pd.concat(dataframes_list, ignore_index=True)
 
     combined_csv_file_name = "combined.csv"
     combined_csv_path = os.path.join(csv_dir_path, combined_csv_file_name)
+    try:
+        os.remove(path = combined_csv_path)
+    except:
+        pass
     combined_data.to_csv(combined_csv_path, index=False, encoding='utf-8')
 
-    print(f"Combined CSV file saved at: {combined_csv_path}")
+    print(f"Combined {len(os.listdir(csv_data_dir_path))} CSV files and saved at: {combined_csv_path}")
 
 if __name__ == "__main__":
 
     csv_dir_path = "csv_data"
-    dataframes_list = []
     combine_csv(csv_dir_path)
